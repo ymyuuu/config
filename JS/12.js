fb0c48ad5788ede97277bec22a1df1629d006774
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-18 21:02:12 +0800

    Update 12.js

--- JS/12.js
+++ JS/12.js
@@ -1,32 +1,89 @@
-const http = require('http');
+// 导入所需的模块
+const axios = require('axios');
+const crypto = require('crypto');
 
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  hostname: 'bs.svv.ink',
-  path: '/index.php',
-  port: 80
-};
+// 设置相关参数
+const phoneOrEmail = 'your_phone_or_email';
+const password = 'your_password';
+const steps = 10000;
 
-const req = http.request(options, res => {
-  let chunks = [];
-  res.on("data", chunk => chunks.push(chunk));
-  res.on("end", () => {
-    const resData = Buffer.concat(chunks).toString('utf-8');
-    console.log(resData);
+// 登录验证
+async function login() {
+  const loginUrl = 'https://account.xiaomi.com/pass/serviceLoginAuth2';
+  const sSecurity = 'pre=';
+  const loginData = {
+    sid: 'mibi',
+    qs: '%3Fsid%3Dmibi',
+    _json: 'true',
+    callback: 'https://account.xiaomi.com',
+    sid: 'mibi',
+    hidden: '',
+    _sign: '',
+    serviceParam: '{"checkSafePhone":false}',
+    service: 'mibi',
+    buttonText: '',
+    user: phoneOrEmail,
+    hash: crypto
+      .createHash('md5')
+      .update(password)
+      .digest('hex')
+  };
+
+  const response = await axios.post(loginUrl, loginData, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
   });
-});
 
-req.on('error', error => {
-  console.error(error);
-});
+  const loginToken = response.headers['set-cookie'][0].split(';')[0];
+  const userId = response.data.userId;
 
-const postData = 'account=' + encodeURIComponent('15033296069') +
-  '&password=' + encodeURIComponent('20030101h') +
-  '&steps=' + encodeURIComponent('80000');
+  return { loginToken, userId };
+}
 
-// 发送请求
-req.write(postData);
-req.end();
+// 获取App Token
+async function getAppToken(loginToken) {
+  const appTokenUrl = 'https://api-user.huami.com/registrations/+86' + phoneOrEmail + '/tokens';
+  const appTokenData = {
+    third_name: 'huami_phone',
+    password: password,
+    captcha: '',
+    redirect_uri: 'https://s3-us-west-2.amazonaws.com/hm-registration/successsignin.html',
+    client_id: 'HuaMi',
+    grant_type: 'password',
+    token: loginToken
+  };
+
+  const response = await axios.post(appTokenUrl, appTokenData);
+  const appToken = response.data.token_info.app_token;
+
+  return appToken;
+}
+
+// 提交模拟的步数数据
+async function submitSteps(appToken) {
+  const submitUrl = 'https://api-mifit-cn.huami.com/v1/data/band_data.json';
+  const submitData = {
+    step: steps,
+    type: 1,
+    source: 2,
+    deviceId: 0,
+    app_token: appToken
+  };
+
+  const response = await axios.post(submitUrl, submitData);
+
+  return response.data;
+}
+
+// 执行主要逻辑
+async function main() {
+  const { loginToken, userId } = await login();
+  const appToken = await getAppToken(loginToken);
+  const submissionResult = await submitSteps(appToken);
+
+  console.log('步数提交结果:', submissionResult);
+}
+
+// 运行主函数
+main();
