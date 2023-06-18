468d91243b6d5de5617d820e8628940613ef2d98
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-18 20:45:13 +0800

    Update 1.js

--- JS/1.js
+++ JS/1.js
@@ -1,28 +1,32 @@
-// 创建XMLHttpRequest对象
-var xhr = new XMLHttpRequest();
+const http = require('http');
 
-// 监听状态改变事件
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        // 请求完成
-        if (xhr.status === 200) {
-            // 响应成功
-            console.log(xhr.responseText);
-        } else {
-            // 响应失败
-            console.log('请求失败：' + xhr.status);
-        }
-    }
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  hostname: 'bs.svv.ink',
+  path: '/index.php',
+  port: 80
 };
 
-// 设置请求信息
-xhr.open('POST', 'http://bs.svv.ink/index.php', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+const req = http.request(options, res => {
+  let chunks = [];
+  res.on("data", chunk => chunks.push(chunk));
+  res.on("end", () => {
+    const resData = Buffer.concat(chunks).toString('utf-8');
+    console.log(resData);
+  });
+});
 
-// 设置请求体参数
-var params = 'account=' + encodeURIComponent('15033296069') +
-    '&password=' + encodeURIComponent('20030101h') +
-    '&steps=' + encodeURIComponent('80000'); // 这里假设步数为10000
+req.on('error', error => {
+  console.error(error);
+});
+
+const postData = 'account=' + encodeURIComponent('15033296069') +
+  '&password=' + encodeURIComponent('20030101h') +
+  '&steps=' + encodeURIComponent('80000');
 
 // 发送请求
-xhr.send(params);
+req.write(postData);
+req.end();
