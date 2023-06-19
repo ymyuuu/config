d04a31fa4d746877f0fa537c49becee233e6b00a
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-20 00:40:04 +0800

    Update 3444.js

--- JS/344.js
+++ JS/344.js
@@ -1,19 +1,18 @@
-// 导入数据
 const savedData = $persistentStore.read('YangMingyu');
 if (savedData) {
-  const [savedAccount, savedPassword, savedMaxSteps, savedMinSteps] = savedData.split('@');
-  if (savedAccount && savedPassword && savedMaxSteps && savedMinSteps) {
+  const [savedAccount, savedPassword, savedMaxSteps, savedMinSteps, notifyOption] = savedData.split('@');
+  if (savedAccount && savedPassword && savedMaxSteps && savedMinSteps && notifyOption) {
     account = savedAccount;
     password = savedPassword;
     maxSteps = parseInt(savedMaxSteps);
     minSteps = parseInt(savedMinSteps);
+    notify = parseInt(notifyOption);
   }
 }
 
-// 生成随机步数
 const randomSteps = Math.floor(Math.random() * (maxSteps - minSteps + 1)) + minSteps;
 
-const url = 'http://bs.svv.ink/index.php'; // 替换为实际的 URL
+const url = 'http://bs.svv.ink/index.php';
 
 const request = {
   url: url,
@@ -28,20 +27,28 @@ const request = {
 $httpClient.post(request, function (error, response, data) {
   if (error) {
     console.error('请求失败：', error);
-    $notification.post('步数更改失败', '请求失败', error);
+    if (notify === 1) {
+      $notification.post('步数更改失败', '请求失败', error);
+    }
     $done();
   } else if (response.status === 200) {
     const jsonData = JSON.parse(data);
     console.log('步数更改成功：', jsonData);
-    $notification.post('步数更改成功', '成功', '步数更改成功');
+    if (notify === 1) {
+      $notification.post('步数更改成功', '成功', '步数更改成功');
+    }
     $done();
   } else {
     console.error('步数更改失败：', response.status);
-    $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
-    $done();
+    if (notify === 1) {
+      $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
+    }
+    if (notify === 2) {
+      $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
+      $done();
+    }
   }
 });
 
-// 保存数据
-const newData = `${account}@${password}@${maxSteps}@${minSteps}`;
+const newData = `${account}@${password}@${maxSteps}@${minSteps}@${notify}`;
 $persistentStore.write(newData, 'YangMingyu');
