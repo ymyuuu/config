a7888cac9814e1f3c27b3472ffb8571e7c3e8428
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-19 22:35:03 +0800

    Update 123.js

--- JS/123.js
+++ JS/123.js
@@ -1,178 +1,96 @@
-const axios = require('axios')
+let { config } = require('../config.js')(runtime, global)
+let fakeWalkData = require('../lib/FakeWalkDataUtil.js')
+let pushClient = require('../lib/PushPlusUtil.js')
+let formatDate = require('../lib/DateUtil.js')
+let singletonRequire = require('../lib/SingletonRequirer.js')(runtime, global)
+let commonFunctions = singletonRequire('CommonFunction')
 
-const headers = {
-  'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-}
+/**
+ * 下载小米运动APP 现在名字叫 Zepp Life
+ * 注册一个账号  手机号可以直接注册 邮箱的需要选择地区为 其他
+ * 这一步非常重要 否则刷的步数无意义：登录后进入APP的第三方接入 绑定到支付宝
+ * 然后在下面的accounts中配置一下用户名密码 手机号的需要增加+86前缀 邮箱的不需要
+ * 然后运行当前脚本即可自动刷步数数据 如果不需要随机的步数将min和max改成相同即可 或者可以将范围改大一些
+ * 运行完毕后APP将会被踢出登录，先试一下 数据能到支付宝之后就可以将APP卸载，后续每天修改步数后都会通过华米自有协议同步到支付宝中不再需要打开APP
+ * 
+ * 对当前脚本设置一个每日自动执行的定时任务即可每天自动刷步数了 建议时间为晚上
+ */
 
-// 账号配置
-const user = '15033296069'; // 账号
-const password = '20030101h'; // 密码
-const STEP = Math.ceil(Math.random() * 10000) + 8000;
+// console.show()
+/**
+ * 配置需要自动生成步数数据的账号 手机号的需要增加+86前缀
+ * min,max 设置一个随机的范围 生成随机的步数
+ */
 
-const dateObj = new Date();
-let dataJson2 = [
-  {
-    "summary": "{\"slp\":{\"ss\":73,\"lt\":304,\"dt\":0,\"st\":1589920140,\"lb\":36,\"dp\":92,\"is\":208,\"rhr\":0,\"stage\":[{\"start\":269,\"stop\":357,\"mode\":2},{\"start\":358,\"stop\":380,\"mode\":3},{\"start\":381,\"stop\":407,\"mode\":2},{\"start\":408,\"stop\":423,\"mode\":3},{\"start\":424,\"stop\":488,\"mode\":2},{\"start\":489,\"stop\":502,\"mode\":3},{\"start\":503,\"stop\":512,\"mode\":2},{\"start\":513,\"stop\":522,\"mode\":3},{\"start\":523,\"stop\":568,\"mode\":2},{\"start\":569,\"stop\":581,\"mode\":3},{\"start\":582,\"stop\":638,\"mode\":2},{\"start\":639,\"stop\":654,\"mode\":3},{\"start\":655,\"stop\":665,\"mode\":2}],\"ed\":1589943900,\"wk\":0,\"wc\":0},\"tz\":\"28800\",\"stp\":{\"runCal\":1,\"cal\":6,\"conAct\":0,\"stage\":[],\"ttl\":"+STEP+",\"dis\":144,\"rn\":0,\"wk\":5,\"runDist\":4,\"ncal\":0},\"v\":5,\"goal\":8000}",
-    "data": [
-        {
-            "stop": 1439,
-            "value": "WhQAUA0AUAAAUAAAUAAAUAAAUAAAWhQAUAYAcBEAUAYAUA8AUAsAUAYAUDIAUCQAUDkAUCkAUD4AUC0AUFcAUD8AUCkAUCEAUCwAUCsAUB4AUCQAUBsAUCcAUBQAUDcAUBoAUCYAUFcAUCAAUDkAUCEAWhQAWhQAWhQAUBAAUEgAUDsAUAgAWhQAUDwAUCEAUAIAUAsAUDoAUD8AWhQAWhQAWhQAWhQAWhQAWhQAAS0QEAsAWhQAAR8SEBcHYC4AUCoAUBMAUAIAUAYAUAsAUCsAUAUAUBIAUBIAUBsAUBgAUAoAUBsAUBUAUBkAUDIAUC0AUC4AUBAAWhQAUCsAUB8AUAIAUB8AUDUAUEEAUDUAUBkAUCYAUEoAUCYAUBIAUCAAUCkAUDAAUB4AUB0AUDEAUCUAUCgAUAQAWhQAUA8AUDwAUB8AUCUAUBQAUB4AUAUAWhQAUAAAUA8AUBkAUCgAUCwAUCkAUCgAYCIAYCIAYCgAUAoAWhQAUBwAWhQAUBoAUDkAUD4AYAkAYAYAWhQAWhQAUB4AWhQAUAQAUBcAUBAAUAUAWhQAUB0AcBYAehQAcBoAehQAehQAehQAcAMAcAMAehQAcAIAehQAcBIAcA0AehQAehQAcAsAcAYAcAEAcAoAehQAehQAcAwAehQAehQAehQAcAEAehQAehQAcAsAehQAehQAcA8AcBkAcAYAcBkAcC0AcAQAcBsAcAMAWhQAUAMAWhQAUBEAUAIAWhQAWhQAWhQAehQAehQAehQAehQAehQAehQAcAAAcB8AcBMAehQAehQAcDkAcBAAcAEAcAMAcAMAcCwAcA8AcAAAcAAAcCIAcAAAcCcAcB4AehQAcAkAehQAcCMAehQAehQAcAoAehQAehQAehQAcBgAcBgAcAkAehQAcAcAcCgAcBQAcA0AcAwAcCcAcCkAcAAAUAAAUAAAUB4AUBwAUAAAUAAAUCkAUBIAUBMAUCgAUA8AUBEAUD0AUCAAYAMAYCkAUBsAUB4AYCgAahQAUBkAWhQAWhQAUCAAUBcAUA8AUBAAUAcAUB8AUCEAUCMAUCkAYAMAYAAAUBsAUBEAUBgAUAUAUB0AUAAAUAAAUAAAUAAAUAAAUAQAUAAAUAAAUAAAUAAAWwAAUAAAcAAAcAAAcAAAcAAAcAAAcAAAcA0AcAAAcAAAcAAAcAIAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcA8AehQAehQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAEAeRMAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAsAcAAAcAAAcAAAcAAAcAAAcAoAcAAAcBMAcAAAcAAAcAAAcAAAcAAAcAAAcA4AcAcAehQAehQAcAAAcAAAcAIAehQAehQAcAAAcAAAcAAAcAAAcAAAcAIAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBcAehQAehQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAehQAcAMAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBUAeQAAcAAAcAAAcAAAcFgAcAAAcAAAcAAAcBkAeQAAcAAAcAAAcAAAcAAAcE0AcAQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAeVAAehQAehQAcAAAcAAAcAAAcAAAcAUAeRwAUAAAUFUAUAAAUAAAUAAAUAAAUAAAUCMAeQAAcAAAcAAAcE0AUAAAUAAAUAAAUAAAUAAAUAAAcAAAcAAAcAAAcE4AcAAAcAAAcAAAcAAAcAgAcBAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAkAcAAAcAAAcAAAcAAAcBwAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAYAcBAAeQAAcB8AeQAAcAAAcAAAcAAAeSoAcAAAcAAAcAAAcAAAcAAAcAsAcAAAeScAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcCAAcAAAUAAAUAAAUAAAUAAAUAAAUBEAehQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBwAehQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBYAcAAAcAAAcAAAcAYAcAAAcAAAcCsAcAAAcAAAcAgAcAAAcAAAcBsAeRQAcAAAcAAAcAEAcAAAcAAAcAAAcAAAcAAAcAAAcA8AcAAAcAAAcBoAcAAAcAEAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBIAcAAAcA0AcBAAcAAAcAAAcAAAcAAAehQAehQAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcCgAcAAAcBkAcAAAcB0AcAAAcAAAcBgAcAAAUAEAUBsAWhQAUB4AWhQAUCkAWQ8AUCsAUA0AWTUAXBAAWhQAUBMAUAQAUAcAUAoAUA8AUBkAUBcAUCoAUAIAUBQAWhQAWhQAUBIAUBQAUAcAWhQAUBYAWhQAUAgAWhQAWhQAUAkAUE0AUHUAAWMTEEcKYDoAYAgAUAMAWhQAUAUAUAYAUAkAUB4AUAsAUAIAUBMAWhQAAVQdAWAlEDYAYCQAUAQAUBgAUAgAUAUAUBQAUAIAWhQAUAkAUAMAUA4AWhQAehQAcAoAcAIAehQAcB0AcCcAUCsAUAEAUAgAUAoAUAIAUAsAUAIAWhQAWhQAUAgAUA0AWhQAUAYAWhQAUAEAWhQAWhQAUBAAUBQAUBIAUBcAUAoAYBAAYAIAAUkZAUglAVYSYBcAYAoAYCAAYAsAUBUAUB0AUBAAUBEAUCAAUBUAUBYAUA0AUB4AUBcAUBsAUBMAUBUAYAsAYAwAYAsAUB4AUBoAUBoAUBoAUBQAUAcAWhQAUBgAUBkAUBsAUBUAUBAAUCAAUCYAUB8AUB4AUBwAUAcAUBsAUBwAUBwAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAA",
-            "did": "DA932FFFFE8816E7",
-            "tz": 32,
-            "src": 17,
-            "start": 0
-        }
-    ],
-    "data_hr": "/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+",
-    "summary_hr": "{\"ct\":0,\"id\":[]}",
-    "date": `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`
+let accounts = config.walking_accounts.map(v => {
+  let randomRange = /(\d+)-(\d+)/
+  let result = randomRange.exec(v.randomRange)
+  let min = 18000, max = 25000
+  if (result && result.length > 2) {
+    min = parseInt(result[1])
+    max = parseInt(result[2])
   }
-]
-
-var handleCircular = function() {  
-  var cache = [];
-  var keyCache = []
-  return function(key, value) {
-      if (typeof value === 'object' && value !== null) {
-          var index = cache.indexOf(value);
-          if (index !== -1) {
-              return '[Circular ' + keyCache[index] + ']';
-          }
-          cache.push(value);
-          keyCache.push(key || 'root');
-      }
-      return value;
-  }
-}
-var tmp = JSON.stringify;  
-JSON.stringify = function(value, replacer, space) {  
-  replacer = replacer || handleCircular();
-  return tmp(value, replacer, space);
-}
-
-// 获取登录code
-function get_code(str){
-  const reg = /(?<=access=).*?(?=&)/;
-  const codeArr = reg.exec(str);
-  return codeArr[0];
-}
-
-async function requestPromise(params){
-  return axios({
-    url: params.url,
-    method: params.method,
-    headers: params.headers || headers,
-    data: new URLSearchParams(params.body),
-    validateStatus: status => {
-      return status >= 200 && status < 400;
-    },
-    maxRedirects: 0
-  })
-  .then(res => {
-    return res;
-  })
-  .catch(err => {
-    console.log(params.url, err)
-    throw Error(err)
-  })
-}
-
-// 登陆
-async function login(user, password){
-  const res1 = await requestPromise({
-    url: `https://api-user.huami.com/registrations/+86${user}/tokens`, 
-    body: {
-      "client_id":"HuaMi",
-      "password": password,
-      "redirect_uri":"https://s3-us-west-2.amazonaws.com/hm-registration/successsignin.html",
-      "token":"access"
-    },
-    headers: {
-      "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8",
-      "User-Agent":"MiFit/4.6.0 (iPhone; iOS 14.0.1; Scale/2.00)"
-    },
-    method: 'POST',
-  })
-  console.log('res1===', res1.headers)
-  const code = get_code(res1.headers.location)
-
-  const res2 = await requestPromise({
-    url: 'https://account.huami.com/v2/client/login',
-    body: {
-      "app_name":"com.xiaomi.hm.health",
-      "app_version":"4.6.0",
-      "code": code,
-      "country_code":"CN",
-      "device_id":"2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
-      "device_model":"phone",
-      "grant_type":"access_token",
-      "third_name":"huami_phone",
-    },
-    method: 'POST',
-  })
-  console.log('res2====', JSON.stringify(res2.data))
-
-  const login_token = res2.data.token_info.login_token;
-  const userid = res2.data.token_info.user_id;
-
   return {
-    login_token,
-    userid,
+    userName: +8615033296069,
+    password: 20030101h,
+    min: 80000,
+    max: 80001
+  }
+})
+let total = accounts.length
+if (total <= 0) {
+  toastLog('无账号，不刷步数')
+}
+let generateResult = []
+accounts.forEach((config, idx) => {
+  let existsData = commonFunctions.getTodaysWalkingData(config.userName)
+  console.verbose('获取缓存中当前账号', config.userName, '的历史刷新步数', existsData)
+  config.min = Math.max(existsData, config.min)
+  let resultStep = generateFakeWalkStep(config)
+  if (resultStep == false) {
+    generateResult.push({ userName: config.userName, result: '修改步数失败，请查看日志' })
+  } else {
+    commonFunctions.updateFakeWalkingData(config.userName, resultStep)
+    generateResult.push({ userName: config.userName, result: '修改步数成功，随机步数：' + resultStep })
+  }
+  if (idx < total - 1) {
+    console.log('延迟五秒钟执行下一个')
+    // 延迟五秒钟
+    sleep(5000)
+  }
+})
+
+if (generateResult.length > 0 && config.pushplus_walking_data && config.pushplus_token) {
+  let content = '<div><span>修改时间：' + formatDate(new Date()) + '</span><br/>'
+  content += '<ul>'
+  content += generateResult.map(result => '<li><span>' + result.userName + '</span>' + result.result + '</li>').join('')
+  content += '</ul></div>'
+  pushClient.pushHtml('小米运动修改步数完成', content)
+}
+
+
+function generateFakeWalkStep (config) {
+  let step = generateRandomStep(config.min, config.max)
+  let result = fakeWalkData(config.userName, config.password, step, device.getAndroidId())
+  if (result) {
+    toastLog('账号' + config.userName + '修改步数成功 随机步数：' + step)
+    return step
+  } else {
+    toastLog('账号' + config.userName + '修改步数失败')
+    return false
   }
 }
 
-exports.main = async () => {
-  let {login_token = 0, userid} = await login(user, password)
-  if(login_token === 0){
-    console.log('登陆失败！');
-    return "login fail!"
+function generateRandomStep (min, max) {
+  min = min || 8000
+  max = max || 15000
+  if (min == max) {
+    return min
   }
-  const t = await get_time();
-  const app_token = await get_app_token(login_token);
-  // const dateObj = new Date();
-  // const date = `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
-  // let data_json = DATAJSON + date + "\"}]";
-  // data_json = data_json.replace('12345', STEP);
-  console.log('data_json=========>>>', JSON.stringify(dataJson2))
-
-  const res = await requestPromise({
-    url: `https://api-mifit-cn.huami.com/v1/data/band_data.json?&t=${t}`,
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.12(0x17000c2d) NetType/WIFI Language/zh_CN',
-      'apptoken': app_token,
-    },
-    body: {
-      'data_json': JSON.stringify(dataJson2),
-      'userid': userid,
-      'device_type': '0',
-      'last_sync_data_time': '1589917081',
-      'last_deviceid': 'DA932FFFFE8816E7',
-    },
-    method: "POST",
-  })
-  console.log('finally request:', res.data)
-  console.log(`改变步数为 ${STEP}, 状态： ${res.data.message}`)
-  return `改变步数为 ${STEP}, 状态： ${res.data.message}`
+  if (min > max) {
+    min = min + max
+    max = min - max
+    min = min - max
+  }
+  return min + parseInt((Math.random() * max) % (max - min))
 }
-
-// 获取时间戳
-async function get_time(){
-  const res = await requestPromise({
-    url: 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp',
-    method: 'GET',
-  });
-  console.log('get_time res===', res.data.data.t)
-  return res.data.data.t;
-}
-
-// 获取app_token
-async function get_app_token(login_token){
-  const res = await requestPromise({
-    url: `https://account-cn.huami.com/v1/client/app_tokens?app_name=com.xiaomi.hm.health&dn=api-user.huami.com%2Capi-mifit.huami.com%2Capp-analytics.huami.com&login_token=${login_token}&os_version=4.1.0`,
-    method: 'GET'
-  })
-  console.log('get_app_token res===', res.data.token_info.app_token)
-  return res.data.token_info.app_token;
-}
-
-// exports.main()
