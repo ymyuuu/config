a125a77742ab57b49e6411aef39b52367bc251e3
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-20 03:11:33 +0800

    Update and rename 1.js to 0.js

--- JS/0.js
+++ JS/0.js
@@ -13,51 +13,37 @@ if (savedData) {
 // 判断账号密码最大步数最小步数是否存在
 if (!account) {
   console.error('缺少账号信息');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '缺少账号信息', '请检查账号');
-  }
+  $notification.post('步数更改失败', '缺少账号信息', '请检查账号');
   $done();
 }
 if (!password) {
   console.error('缺少密码信息');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '缺少密码信息', '请检查密码');
-  }
+  $notification.post('步数更改失败', '缺少密码信息', '请检查密码');
   $done();
 }
 if (!maxSteps) {
   console.error('缺少最大步数信息');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数');
-  }
+  $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数');
   $done();
 }
 if (!minSteps) {
   console.error('缺少最小步数信息');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数');
-  }
+  $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数');
   $done();
 }
 
 // 判断最大步数和最小步数是否超限
 if (maxSteps > 98000 || minSteps > 98000) {
   console.log('最大步数和最小步数不能超过98000');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '最大步数和最小步数不能超过98000', '请检查最大步数和最小步数');
-  }
+  $notification.post('步数更改失败', '最大步数和最小步数不能超过98000', '请检查最大步数和最小步数');
   $done();
 } else if (maxSteps < minSteps) {
   console.log('最大步数不能小于最小步数');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '最大步数不能小于最小步数', '请检查最大步数和最小步数');
-  }
+  $notification.post('步数更改失败', '最大步数不能小于最小步数', '请检查最大步数和最小步数');
   $done();
 } else if (minSteps > maxSteps) {
   console.log('最小步数不能大于最大步数');
-  if (notify === 1 || notify === 3) {
-    $notification.post('步数更改失败', '最小步数不能大于最大步数', '请检查最大步数和最小步数');
-  }
+  $notification.post('步数更改失败', '最小步数不能大于最大步数', '请检查最大步数和最小步数');
   $done();
 } else {
   const randomSteps = Math.floor(Math.random() * (maxSteps - minSteps + 1)) + minSteps;
@@ -83,26 +69,23 @@ if (maxSteps > 98000 || minSteps > 98000) {
   $httpClient.post(request, function (error, response, data) {
     if (error) {
       console.error('请求失败：', error);
-      if (notify === 1 || notify === 3) {
+      if ([1, 3].includes(notify)) {
         $notification.post('步数更改失败', '请求失败', error);
       }
       $done();
     } else if (response.status === 200) {
       const jsonData = JSON.parse(data);
       console.log(`Steps Update Successful: ${randomSteps.toString()}`, jsonData);
-      if (notify === 1 || notify === 2) {
+      if ([1, 2].includes(notify)) {
         $notification.post('Steps Update Successful', `Steps: ${randomSteps.toString()}`, '@YangMingyu', 'https://t.me/ymyuuu');
       }
       $done();
     } else {
       console.error('步数更改失败：', response.status);
-      if (notify === 1 || notify === 3) {
+      if ([1, 3].includes(notify)) {
         $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
       }
-      if (notify === 2) {
-        $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
-        $done();
-      }
+      $done();
     }
   });
 
