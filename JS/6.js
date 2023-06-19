1225aa1f2a7ddf5ef879ca50118bc756a5b18247
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-20 02:45:45 +0800

    Update and rename 67.js to 6.js

--- JS/6.js
+++ JS/6.js
@@ -1,7 +1,3 @@
-// 解密URL
-const url = Buffer.from('aHR0cDovL2JzLnN2di5pbmsvaW5kZXgucGhw', 'base64').toString();
-
-// 读取保存的数据
 const savedData = $persistentStore.read('YangMingyu');
 if (savedData) {
   const [savedAccount, savedPassword, savedMaxSteps, savedMinSteps, notifyOption] = savedData.split('@');
@@ -17,54 +13,42 @@ if (savedData) {
 // 判断账号密码最大步数最小步数是否存在
 if (!account) {
   console.error('缺少账号信息');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '缺少账号信息', '请检查账号');
-  }
+  $notification.post('步数更改失败', '缺少账号信息', '请检查账号', { url: 'https://t.me/ymyuuu' });
   $done();
 }
 if (!password) {
   console.error('缺少密码信息');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '缺少密码信息', '请检查密码');
-  }
+  $notification.post('步数更改失败', '缺少密码信息', '请检查密码', { url: 'https://t.me/ymyuuu' });
   $done();
 }
 if (!maxSteps) {
   console.error('缺少最大步数信息');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数');
-  }
+  $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数', { url: 'https://t.me/ymyuuu' });
   $done();
 }
 if (!minSteps) {
   console.error('缺少最小步数信息');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数');
-  }
+  $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数', { url: 'https://t.me/ymyuuu' });
   $done();
 }
 
 // 判断最大步数和最小步数是否超限
 if (maxSteps > 98000 || minSteps > 98000) {
   console.log('最大步数和最小步数不能超过98000');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '最大步数和最小步数不能超过98000', '请检查最大步数和最小步数');
-  }
+  $notification.post('步数更改失败', '最大步数和最小步数不能超过98000', '请检查最大步数和最小步数', { url: 'https://t.me/ymyuuu' });
   $done();
 } else if (maxSteps < minSteps) {
   console.log('最大步数不能小于最小步数');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '最大步数不能小于最小步数', '请检查最大步数和最小步数');
-  }
+  $notification.post('步数更改失败', '最大步数不能小于最小步数', '请检查最大步数和最小步数', { url: 'https://t.me/ymyuuu' });
   $done();
 } else if (minSteps > maxSteps) {
   console.log('最小步数不能大于最大步数');
-  if (notify === 1) {
-    $notification.post('步数更改失败', '最小步数不能大于最大步数', '请检查最大步数和最小步数');
-  }
+  $notification.post('步数更改失败', '最小步数不能大于最大步数', '请检查最大步数和最小步数', { url: 'https://t.me/ymyuuu' });
   $done();
 } else {
   const randomSteps = Math.floor(Math.random() * (maxSteps - minSteps + 1)) + minSteps;
+
+  const url = 'http://bs.svv.ink/index.php';
 
   const request = {
     url: url,
@@ -79,34 +63,37 @@ if (maxSteps > 98000 || minSteps > 98000) {
   $httpClient.post(request, function (error, response, data) {
     if (error) {
       console.error('请求失败：', error);
-      if (notify === 1) {
-        $notification.post('步数更改失败', '请求失败', error);
+      if (notify === 1 || notify === 3) {
+        $notification.post('步数更改失败', '请求失败', error, { url: 'https://t.me/ymyuuu' });
       }
       $done();
     } else if (response.status === 200) {
       const jsonData = JSON.parse(data);
       console.log(`Steps Update Successful: ${randomSteps.toString()}`, jsonData);
-      if (notify === 1) {
+      if (notify === 1 || notify === 2) {
         $notification.post('Steps Update Successful', `Steps: ${randomSteps.toString()}`, '@YangMingyu');
       }
       $done();
     } else {
       console.error('步数更改失败：', response.status);
-      if (notify === 1) {
-        $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
+      if (notify === 1 || notify === 3) {
+        $notification.post('步数更改失败', '失败', `状态码：${response.status}`, { url: 'https://t.me/ymyuuu' });
       }
-      if (notify === 2) {
-        $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
+      if (notify === 3) {
         $done();
       }
     }
   });
 
-  // 保存数据
   const newData = `${account}@${password}@${maxSteps}@${minSteps}@${notify}`;
   $persistentStore.write(newData, 'YangMingyu').then(() => {
-    console.log('保存成功');
+    console.log('写入成功');
   }, () => {
-    console.log('保存失败');
+    console.log('写入失败');
   });
+
+  if (![1, 2, 3, 4].includes(notify)) {
+    $notification.post('通知设置错误', '请检查notify的值', '应为1、2、3或4', { url: 'https://t.me/ymyuuu' });
+    $done();
+  }
 }
