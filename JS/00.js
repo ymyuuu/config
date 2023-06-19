0be628eeee8924614ce7767e8ddb04ab4ee26c3e
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-20 03:14:05 +0800

    Update and rename 0.js to 00.js

--- JS/00.js
+++ JS/00.js
@@ -13,37 +13,51 @@ if (savedData) {
 // 判断账号密码最大步数最小步数是否存在
 if (!account) {
   console.error('缺少账号信息');
-  $notification.post('步数更改失败', '缺少账号信息', '请检查账号');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '缺少账号信息', '请检查账号');
+  }
   $done();
 }
 if (!password) {
   console.error('缺少密码信息');
-  $notification.post('步数更改失败', '缺少密码信息', '请检查密码');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '缺少密码信息', '请检查密码');
+  }
   $done();
 }
 if (!maxSteps) {
   console.error('缺少最大步数信息');
-  $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数');
+  }
   $done();
 }
 if (!minSteps) {
   console.error('缺少最小步数信息');
-  $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数');
+  }
   $done();
 }
 
 // 判断最大步数和最小步数是否超限
 if (maxSteps > 98000 || minSteps > 98000) {
   console.log('最大步数和最小步数不能超过98000');
-  $notification.post('步数更改失败', '最大步数和最小步数不能超过98000', '请检查最大步数和最小步数');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '最大步数和最小步数不能超过98000', '请检查最大步数和最小步数');
+  }
   $done();
 } else if (maxSteps < minSteps) {
   console.log('最大步数不能小于最小步数');
-  $notification.post('步数更改失败', '最大步数不能小于最小步数', '请检查最大步数和最小步数');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '最大步数不能小于最小步数', '请检查最大步数和最小步数');
+  }
   $done();
 } else if (minSteps > maxSteps) {
   console.log('最小步数不能大于最大步数');
-  $notification.post('步数更改失败', '最小步数不能大于最大步数', '请检查最大步数和最小步数');
+  if (notify !== 4) {
+    $notification.post('步数更改失败', '最小步数不能大于最大步数', '请检查最大步数和最小步数');
+  }
   $done();
 } else {
   const randomSteps = Math.floor(Math.random() * (maxSteps - minSteps + 1)) + minSteps;
@@ -59,12 +73,6 @@ if (maxSteps > 98000 || minSteps > 98000) {
     },
     body: `account=${account}&password=${password}&steps=${randomSteps}&max_steps=${maxSteps}&min_steps=${minSteps}`,
   };
-
-  if (![1, 2, 3, 4].includes(notify)) {
-    console.error('无效的通知策略');
-    $notification.post('步数更改失败', '无效的通知策略', '请检查通知选项');
-    $done();
-  }
 
   $httpClient.post(request, function (error, response, data) {
     if (error) {
@@ -85,7 +93,9 @@ if (maxSteps > 98000 || minSteps > 98000) {
       if ([1, 3].includes(notify)) {
         $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
       }
-      $done();
+      if (notify !== 4) {
+        $done();
+      }
     }
   });
 
