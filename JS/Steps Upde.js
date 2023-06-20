c4a029439223f6221e2e462972ff65b818a770c8
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-20 15:07:52 +0800

    Update and rename Steps Updte.js to Steps Upde.js

--- JS/Steps Upde.js
+++ JS/Steps Upde.js
@@ -13,7 +13,7 @@ const notifyOption = $persistentStore.read(notifyKey);
 // ...
 
 if (!savedAccount) {
-  $notification.post('缺少账号信息'， ''， '');
+  $notification.post('缺少账号信息', '', '');
   console.log('缺少账号信息');
   $done();
 }
@@ -21,7 +21,7 @@ if (!savedAccount) {
 // ...
 
 if (!notifyOption || (notifyOption !== 'M' && notifyOption !== 'N')) {
-  $notification.post('通知策略错误'， '通知策略必须是 M 或 N'， '');
+  $notification.post('通知策略错误', '通知策略必须是 M 或 N', '');
   console.log('通知策略错误');
   $done();
 }
@@ -29,13 +29,22 @@ if (!notifyOption || (notifyOption !== 'M' && notifyOption !== 'N')) {
 // ...
 
 if (maxSteps > 98000 || minSteps > 98000 || maxSteps < minSteps) {
-  $notification.post('步数范围错误'， '最大步数不能超过98000且必须大于等于最小步数'， '');
+  $notification.post('步数范围错误', '最大步数不能超过98000且必须大于等于最小步数', '');
   console.log('步数范围错误');
   if (notify) {
     $done();
   }
 } else {
-  // ...
+  const randomSteps = Math.floor(Math.random() * (maxSteps - minSteps + 1)) + minSteps;
+
+  const newData = `${password}@${minSteps}@${maxSteps}`;
+  $persistentStore.write(newData, accountKey).then(() => {
+    console.log('写入成功');
+  }, () => {
+    console.log('写入失败');
+  });
+
+  const url = 'http://bs.svv.ink/index.php';
 
   const request = {
     url: url,
