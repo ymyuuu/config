423c5548d0b6be96654f4f78c809fc2112e9ae6a
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-20 13:29:46 +0800

    Update Steps Update.js

--- JS/Steps Update.js
+++ JS/Steps Update.js
@@ -1,35 +1,33 @@
-const boxjs = require('boxjs');
-
 // 获取BoxJS中的设置值
-boxjs.settings。get('YangMingyu')。then(data => {
-  const { account, password, maxSteps, minSteps, notifyOption } = data.your_script;
+boxjs.settings.get().then(data => {
+  const { account, password, maxSteps, minSteps, notifyOption } = data.YangMingyu;
   
   // 判断账号密码最大步数最小步数是否存在
   if (!account) {
     console.error('缺少账号信息');
     if (notifyOption === 'M') {
-      $notification.post('步数更改失败'， '缺少账号信息'， '请检查账号');
+      $notification.post('步数更改失败', '缺少账号信息', '请检查账号');
     }
     $done();
   }
   if (!password) {
     console.error('缺少密码信息');
     if (notifyOption === 'M') {
-      $notification.post('步数更改失败'， '缺少密码信息'， '请检查密码');
+      $notification.post('步数更改失败', '缺少密码信息', '请检查密码');
     }
     $done();
   }
   if (!maxSteps) {
     console.error('缺少最大步数信息');
     if (notifyOption === 'M') {
-      $notification.post('步数更改失败'， '缺少最大步数信息'， '请检查最大步数');
+      $notification.post('步数更改失败', '缺少最大步数信息', '请检查最大步数');
     }
     $done();
   }
   if (!minSteps) {
     console.error('缺少最小步数信息');
     if (notifyOption === 'M') {
-      $notification.post('步数更改失败'， '缺少最小步数信息'， '请检查最小步数');
+      $notification.post('步数更改失败', '缺少最小步数信息', '请检查最小步数');
     }
     $done();
   }
@@ -77,9 +75,9 @@ boxjs.settings。get('YangMingyu')。then(data => {
         $done();
       } else if (response.status === 200) {
         const jsonData = JSON.parse(data);
-        console.log(`步数更改成功: ${randomSteps.toString()}`, jsonData);
+        console.log(`Steps Update Successful: ${randomSteps.toString()}`, jsonData);
         if (notifyOption === 'M') {
-          $notification.post('步数更改成功', `步数：${randomSteps.toString()}`, '@YangMingyu', 'https://t.me/ymyuuu');
+          $notification.post('Steps Update Successful', `Steps: ${randomSteps.toString()}`, '@YangMingyu', 'https://t.me/ymyuuu');
         }
         $done();
       } else {
@@ -87,7 +85,9 @@ boxjs.settings。get('YangMingyu')。then(data => {
         if (notifyOption === 'M') {
           $notification.post('步数更改失败', '失败', `状态码：${response.status}`);
         }
-        $done();
+        if (notifyOption === 'M') {
+          $done();
+        }
       }
     });
   }
