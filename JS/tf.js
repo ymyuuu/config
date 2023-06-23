895de7aeaf0e62dce30c1727e98e08f0d1baf13b
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-23 23:04:01 +0800

    Update tf.js

--- JS/tf.js
+++ JS/tf.js
@@ -56,12 +56,12 @@ const shouldDeleteKeys = $.getdata('是否删除已检测到的appkey') === '是
         if (!fullstr.test(dataStr)) {
           result[name] = {
             has: true,
-            context: '抢到👆点击立刻跳转TestFlight'
+            context: '🈶️👆点击立刻跳转TestFlight'
           }
         } else {
           result[name] = {
             has: false,
-            context: '无位' 
+            context: '🈚️👆' 
           }
         }
         resolve(result)
