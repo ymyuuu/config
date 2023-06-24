a7d7ac7d5c2d55962d0fd83ceb1273115a4cec02
YangMingyu <135582157+Y-7-0101@users.noreply.github.com>
2023-06-24 14:13:03 +0800

    Update test.js

--- JS/test.js
+++ JS/test.js
@@ -1,10 +1,4 @@
-/****************************
-支持将Loon重写解析至Loon Stash Surge Shadowrocket
-说明
-原脚本作者@小白脸 脚本修改@chengkongyiban
-感谢@xream 提供的echo-response.js
-插件图标用的 @Keikinn 的 StickerOnScreen项目 以及 @Toperlock 的图标库项目，感谢
-***************************/
+
 const isStashiOS = 'undefined' !== typeof $environment && $environment['stash-version'];
 const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
 const isShadowrocket = 'undefined' !== typeof $rocket;
