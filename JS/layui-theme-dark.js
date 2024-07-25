d8e8130ec166662e863db6bd7684a7193c7b2af3
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-07-25 20:09:46 +0800

    Update layui-theme-dark.js

--- JS/layui-theme-dark.js
+++ JS/layui-theme-dark.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded'， function() {
-	// 检测用户的系统主题偏好
-	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')。matches) {
-		// 动态引入 layui-theme-dark.css
-		var link = document.createElement('link');
-		link.rel = 'stylesheet';
-		link.href = 'https://unpkg.com/layui-theme-dark/dist/layui-theme-dark.css';
-		document.head.appendChild(link);
-	}
+document.addEventListener('DOMContentLoaded', function() {
+    // 检测用户的系统主题偏好
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // 动态引入 layui-theme-dark.css
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'https://unpkg.com/layui-theme-dark/dist/layui-theme-dark.css';
+        document.head.appendChild(link);
+    }
 });
